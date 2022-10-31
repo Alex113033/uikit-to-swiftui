af79ac72b20f3e567d045ef31a4bd91ddd27e8a4
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-10-31 23:56:49 +0300

    Initial Commit

---
+++ uikit to swiftui/uikit_to_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uikit_to_swiftuiApp.swift
+//  uikit to swiftui
+//
+//  Created by Александр on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct uikit_to_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
